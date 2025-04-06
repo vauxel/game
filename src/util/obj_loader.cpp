885ff2777832fd9efe20d0c3e2e6b707a500372e
Austin Villee <vauxel@protonmail.com>
2025-04-05 22:50:23 -0500

    WIP triangulation

--- src/util/obj_loader.cpp
+++ src/util/obj_loader.cpp
@@ -65,7 +65,7 @@ int OBJLoader::loadOBJFile(const char* path) {
       float y = parseFloat(&token);
       float z = parseFloat(&token);
 
-      currMesh->rawVertices.push_back(glm::vec3(x, y, z));
+      currMesh->rawVertices.emplace_back(glm::vec3(x, y, z));
 
       continue;
     }
@@ -77,7 +77,7 @@ int OBJLoader::loadOBJFile(const char* path) {
       float x = parseFloat(&token);
       float y = parseFloat(&token);
 
-      currMesh->rawUVs.push_back(glm::vec2(x, y));
+      currMesh->rawUVs.emplace_back(glm::vec2(x, y));
 
       continue;
     }
@@ -90,7 +90,7 @@ int OBJLoader::loadOBJFile(const char* path) {
       float y = parseFloat(&token);
       float z = parseFloat(&token);
 
-      currMesh->rawNormals.push_back(glm::vec3(x, y, z));
+      currMesh->rawNormals.emplace_back(glm::vec3(x, y, z));
 
       continue;
     }
@@ -100,38 +100,50 @@ int OBJLoader::loadOBJFile(const char* path) {
       token += 1;
       token += strspn(token, " \t");
 
-      std::vector<glm::vec3> faceTriples;
+      std::vector<FaceTripleData> faceTriples;
 
       while (token[0] != '\0') {
         int vIdx = 0, uvIdx = 0, normIdx = 0;
 
         if (this->parseFaceTriple(&token, vIdx, uvIdx, normIdx)) {
-          faceTriples.push_back(glm::vec3(vIdx, uvIdx, normIdx));
+          faceTriples.push_back({ vIdx, uvIdx, normIdx });
           token += strspn(token, " \t");
         }
       }
 
-      if (faceTriples.size() > 3) {
-        // TODO: Do triangulation
+      const size_t numFaceTriples = faceTriples.size();
+
+      if (numFaceTriples < 3) {
+        LOG_WARNING("Degenerated face found in OBJ file \"%s\" on line %lu", path, lineNum);
+        continue;
+      }
+
+      if (numFaceTriples > 3) {
+        this->triangulateFace(currMesh, faceTriples);
+
+        if (faceTriples.size() % 3 != 0) {
+          LOG_WARNING("Failed to triangulate %lu-gon face in OBJ file \"%s\" on line %lu", numFaceTriples, path, lineNum);
+          continue;
+        }
       }
 
       for (size_t ti = 0; ti < faceTriples.size(); ti++) {
         unsigned int vertexIndex = 0;
         this->resolveVertex(currMesh, faceTriples[ti], vertexIndex);
-        currMesh->indices.push_back(vertexIndex);
+        currMesh->indices.emplace_back(vertexIndex);
       }
 
       continue;
     }
   }
 
-  this->meshes.push_back(currMesh);
+  this->meshes.emplace_back(currMesh);
 
   fileStream.close();
   return 0;
 }
 
-float OBJLoader::parseFloat(const char** token) {
+inline float OBJLoader::parseFloat(const char** token) {
   (*token) += strspn(*token, " \t");
   float i = atof(*token);
   (*token) += strcspn(*token, " \t");
@@ -174,11 +186,11 @@ bool OBJLoader::parseFaceTriple(const char** token, int& vIdx, int& uvIdx, int& 
   return true;
 }
 
-void OBJLoader::resolveVertex(MeshData* mesh, glm::vec3& originalIndices, unsigned int& vertexIndex) {
+void OBJLoader::resolveVertex(MeshData* mesh, FaceTripleData& originalIndices, unsigned int& vertexIndex) {
   Vertex newVertex = Vertex(
-    mesh->rawVertices[originalIndices.x - 1],
-    originalIndices.y != 0 ? mesh->rawUVs[originalIndices.y - 1] : glm::vec3(0, 0, 0),
-    originalIndices.z != 0 ? mesh->rawNormals[originalIndices.z - 1] : glm::vec3(0, 0, 0)
+    mesh->rawVertices[originalIndices.vertIndex - 1],
+    originalIndices.uvIndex != 0 ? mesh->rawUVs[originalIndices.uvIndex - 1] : glm::vec3(0, 0, 0),
+    originalIndices.normIndex != 0 ? mesh->rawNormals[originalIndices.normIndex - 1] : glm::vec3(0, 0, 0)
   );
 
   for (size_t i = 0; i < mesh->vertices.size(); i++) {
@@ -188,12 +200,281 @@ void OBJLoader::resolveVertex(MeshData* mesh, glm::vec3& originalIndices, unsign
     }
   }
 
-  mesh->vertices.push_back(newVertex);
+  mesh->vertices.emplace_back(newVertex);
   vertexIndex = mesh->vertices.size() - 1;
 }
 
+void OBJLoader::triangulateFace(MeshData* mesh, std::vector<FaceTripleData>& faceTriples) {
+  std::vector<glm::vec3> polyPoints;
+
+  for (size_t i = 0; i < faceTriples.size(); i++) {
+    polyPoints.emplace_back(mesh->rawVertices[faceTriples[i].vertIndex - 1]);
+  }
+
+  const glm::vec3 normal = this->calcPolyNormal(polyPoints);
+
+  if (this->isConvexPoly(polyPoints, normal)) {
+    this->triangulateFaceFan(faceTriples);
+  } else {
+    this->triangulateFaceEarcut(faceTriples, polyPoints, normal);
+  }
+}
+
+inline glm::vec3 OBJLoader::calcPolyNormal(const std::vector<glm::vec3>& points) {
+  glm::vec3 normal;
+  const size_t n = points.size();
+
+  for (size_t i = 0; i < n; i++) {
+    const glm::vec3& curr = points[i % n];
+    const glm::vec3& next = points[(i + 1) % n];
+
+    normal.x += (next.y - curr.y) * (next.z + curr.z);
+    normal.y += (next.z - curr.z) * (next.x + curr.x);
+    normal.z += (next.x - curr.x) * (next.y + curr.y);
+  }
+
+  return glm::normalize(normal);
+}
+
+inline OBJLoader::TurnDirection OBJLoader::calcPolyTurnDir(const glm::vec3& prev, const glm::vec3& curr, const glm::vec3& next, const glm::vec3& normal) {
+  const float dot = glm::dot(glm::cross(next - prev, glm::normalize(curr - prev)), normal);
+
+  if (dot > +0.001) {
+    return TurnDirection::Right;
+  } else if (dot < -0.001) {
+    return TurnDirection::Left;
+  } else {
+    return TurnDirection::NoTurn;
+  }
+}
+
+bool OBJLoader::isConvexPoly(const std::vector<glm::vec3>& points, const glm::vec3& normal) {
+  const size_t n = points.size();
+  TurnDirection polyTurnDir = TurnDirection::NoTurn;
+
+  for (size_t i = 0; i < n; i++) {
+    const glm::vec3& prev = points[(i - 1 + n) % n];
+    const glm::vec3& curr = points[i % n];
+    const glm::vec3& next = points[(i + 1) % n];
+
+    TurnDirection turnDir = this->calcPolyTurnDir(prev, curr, next, normal);
+
+    if (turnDir == TurnDirection::NoTurn) {
+      continue;
+    }
+
+    if (polyTurnDir == TurnDirection::NoTurn) {
+      polyTurnDir = turnDir;
+    }
+
+    if (polyTurnDir != turnDir) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+void OBJLoader::triangulateFaceFan(std::vector<FaceTripleData>& faceTriples) {
+  std::vector<FaceTripleData> newFaceTriples;
+
+  for (size_t i = 1; i < faceTriples.size() - 1; i++) {
+    newFaceTriples.emplace_back(faceTriples[0]);
+    newFaceTriples.emplace_back(faceTriples[i]);
+    newFaceTriples.emplace_back(faceTriples[i + 1]);
+  }
+
+  faceTriples = std::move(newFaceTriples);
+}
+
+void OBJLoader::triangulateFaceEarcut(std::vector<FaceTripleData>& faceTriples, std::vector<glm::vec3>& polyPoints, const glm::vec3& normalPoint) {
+  std::vector<FaceTripleData> newFaceTriples;
+
+  // Calculate and align clockwise-ness
+  {
+    const size_t n = polyPoints.size();
+    double orientationSum = 0.0;
+
+    for (size_t i = 0; i < n; i++) {
+      const glm::vec3& prev = polyPoints[(i - 1 + n) % n];
+      const glm::vec3& curr = polyPoints[i % n];
+      const glm::vec3& next = polyPoints[(i + 1) % n];
+
+      const glm::vec3 cross = glm::cross(curr - prev, next - curr);
+      orientationSum += glm::dot(cross, normalPoint);
+    }
+
+    bool isClockwise = orientationSum < 0.0;
+
+    if (!isClockwise) {
+      faceTriples = {faceTriples.rbegin(), faceTriples.rend()};
+      polyPoints = {polyPoints.rbegin(), polyPoints.rend()};
+    }
+  }
+
+  while (!polyPoints.empty()) {
+    int earIndex = -1;
+
+    // Get biggest ear
+    {
+      const size_t n = polyPoints.size();
+      
+      if (n == 3) {
+        earIndex = 0;
+      } else if (n == 0) {
+        earIndex = -1;
+      } else {
+        double maxArea = DBL_MIN;
+
+        for (size_t i = 0; i < n; i++) {
+          bool isEar = true;
+
+          // Detect if ear
+          {
+            const size_t n = polyPoints.size();
+
+            if (n < 3) {
+              isEar = false;
+            } else if (n == 3) {
+              isEar = true;
+            } else {
+              const int prevIndex = (i - 1 + n) % n;
+              const int currIndex = i % n;
+              const int nextIndex = (i + 1) % n;
+
+              const glm::vec3& prev = polyPoints[prevIndex];
+              const glm::vec3& curr = polyPoints[currIndex];
+              const glm::vec3& next = polyPoints[nextIndex];
+
+              if (this->calcPolyTurnDir(prev, curr, next, normalPoint) != TurnDirection::Right) {
+                isEar = false;
+              } else {
+                for (size_t j = 0; j < polyPoints.size(); j++) {
+                  if (j == prevIndex || j == currIndex || j == nextIndex) {
+                    continue;
+                  }
+
+                  bool pointInsideOrEdgeTriangle = false;
+
+                  // Detect if the point is inside or edge of the triangle
+                  {
+                    static double zero = std::numeric_limits<double>::epsilon();
+
+                    const glm::vec3 v0 = next - prev;
+                    const glm::vec3 v1 = curr - prev;
+                    const glm::vec3 v2 = polyPoints[j] - prev;
+
+                    const float dot00 = glm::dot(v0, v0);
+                    const float dot01 = glm::dot(v0, v1);
+                    const float dot02 = glm::dot(v0, v2);
+                    const float dot11 = glm::dot(v1, v1);
+                    const float dot12 = glm::dot(v1, v2);
+
+                    const float denom = dot00 * dot11 - dot01 * dot01;
+
+                    if (!(std::abs(denom) < zero)) {
+                      const double invDenom = 1.0 / denom;
+                      const double u = (dot11 * dot02 - dot01 * dot12) * invDenom;
+                      const double v = (dot00 * dot12 - dot01 * dot02) * invDenom;
+
+                      pointInsideOrEdgeTriangle = (u >= 0.0) && (v >= 0.0) && (u + v < 1.0);
+                    }
+                  }
+
+                  if (pointInsideOrEdgeTriangle) {
+                    isEar = false;
+                    break;
+                  }
+                }
+              }
+            }
+          }
+
+          if (!isEar) {
+            continue;
+          }
+
+          const glm::vec3& prev = polyPoints[(i - 1 + n) % n];
+          const glm::vec3& curr = polyPoints[i % n];
+          const glm::vec3& next = polyPoints[(i + 1) % n];
+
+          const auto cross = glm::cross(curr - prev, next - prev);
+          const float area = (cross.x * cross.x + cross.y * cross.y + cross.z * cross.z) / 4.0f;
+
+          if (area > maxArea) {
+            earIndex = i;
+            maxArea = area;
+          }
+        }
+      }
+    }
+
+    // Fallback to getting overlapping ear
+    if (earIndex == -1) {
+      const size_t n = polyPoints.size();
+
+      if (n == 3) {
+        earIndex = 0;
+      } else if (n == 0) {
+        earIndex = -1;
+      } else {
+        for (size_t i = 0; i < n; i++) {
+          const glm::vec3& prev = polyPoints[(i - 1 + n) % n];
+          const glm::vec3& curr = polyPoints[i % n];
+          const glm::vec3& next = polyPoints[(i + 1) % n];
+
+          if (this->calcPolyTurnDir(prev, curr, next, normalPoint) != TurnDirection::NoTurn) {
+            continue;
+          }
+
+          if (glm::dot(glm::normalize(curr - prev), glm::normalize(next - curr)) < 0.0) {
+            earIndex = i;
+            break;
+          }
+        }
+      }
+    }
+
+    if (earIndex == -1) {
+      faceTriples.clear();
+      return;
+    }
+
+    const size_t n = faceTriples.size();
+
+    const int prevIndex = (earIndex - 1 + n) % n;
+    const int currIndex = earIndex % n;
+    const int nextIndex = (earIndex + 1) % n;
+
+    // const glm::vec3& prevPoint = polyPoints[prevIndex];
+    // const glm::vec3& currPoint = polyPoints[currIndex];
+    // const glm::vec3& nextPoint = polyPoints[nextIndex];
+
+    const FaceTripleData& prevFaceTriple = faceTriples[prevIndex];
+    const FaceTripleData& currFaceTriple = faceTriples[currIndex];
+    const FaceTripleData& nextFaceTriple = faceTriples[nextIndex];
+
+    newFaceTriples.emplace_back(prevFaceTriple);
+    newFaceTriples.emplace_back(currFaceTriple);
+    newFaceTriples.emplace_back(nextFaceTriple);
+    
+    polyPoints.erase(polyPoints.begin() + earIndex);
+    faceTriples.erase(faceTriples.begin() + earIndex);
+
+    if (polyPoints.size() < 3) {
+      break;
+    }
+  }
+
+  if (polyPoints.size() == 2) {
+    faceTriples = std::move(newFaceTriples);
+  } else {
+    faceTriples.clear();
+  }
+}
+
 void OBJLoader::flushMeshData(MeshData** mesh, std::vector<MeshData*>& meshes) {
-  meshes.push_back(*mesh);
+  meshes.emplace_back(*mesh);
   *mesh = new MeshData();
 }
 
--- src/util/obj_loader.h
+++ src/util/obj_loader.h
@@ -4,17 +4,26 @@
 #include <fstream>
 #include <string>
 #include <cstring>
+#include <cfloat>
 #include <sstream>
 #include <vector>
 #include <glm/vec3.hpp>
 #include <glm/vec2.hpp>
+#include <glm/geometric.hpp>
 #include <mapbox/earcut.hpp>
+#include "logger.h"
 #include "model/vertex.h"
 
 #define IS_SPACE(x) (x == ' ' || x == '\t')
 
 class OBJLoader {
   private:
+    enum class TurnDirection {
+      Right  = 1,
+      Left   = -1,
+      NoTurn = 0
+    };
+
     const char* error = nullptr;
 
     struct MeshData {
@@ -27,12 +36,24 @@ class OBJLoader {
       std::string mtlName = "";
     };
 
+    struct FaceTripleData {
+      int vertIndex;
+      int uvIndex;
+      int normIndex;
+    };
+
     std::string mtlLibName = "";
     std::vector<MeshData*> meshes;
 
     float parseFloat(const char** token);
     bool parseFaceTriple(const char** token, int& vIdx, int& uvIdx, int& normIdx);
-    void resolveVertex(MeshData* mesh, glm::vec3& originalIndices, unsigned int& vertexIndex);
+    void resolveVertex(MeshData* mesh, FaceTripleData& originalIndices, unsigned int& vertexIndex);
+    void triangulateFace(MeshData* mesh, std::vector<FaceTripleData>& faceTriples);
+    void triangulateFaceFan(std::vector<FaceTripleData>& faceTriples);
+    void triangulateFaceEarcut(std::vector<FaceTripleData>& faceTriples, std::vector<glm::vec3>& polyPoints, const glm::vec3& normalPoint);
+    bool isConvexPoly(const std::vector<glm::vec3>& points, const glm::vec3& normal);
+    glm::vec3 calcPolyNormal(const std::vector<glm::vec3>& points);
+    OBJLoader::TurnDirection calcPolyTurnDir(const glm::vec3& prev, const glm::vec3& curr, const glm::vec3& next, const glm::vec3& normal);
     void flushMeshData(MeshData** mesh, std::vector<MeshData*>& meshes);
   public:
     int loadOBJFile(const char* path);
